6cd5433d3398889dd1b9c53a46a690f5f8542528
Anton <=>
2025-10-08 00:07:19 +0300

    scriptDuzenlendi

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
+/* ulaşıp değiştirmek istediğimiz classlarımızı değişkenlerini oluştuyoruz.
+css de bulunan elementlerim progress butonlar prev ve next.. şeklinde gdiyor.
+sadece circles da querySelectorAll kullandım çünkü birden fazla var. */
+
 const progress = document.getElementById('progress')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
-let currentActive = 1
+let currentActive = 1 // bu değişkeni active basamak için kullandım.
 
+/* next değişkenime event listener ekleyip tıklama olayı gerçekleştiğinde basamağı
+yani currentActive'i bir arttırıyorum yani 2 oluyor. */
 next.addEventListener('click', ()=>{
     currentActive++
 
+/* burada circle nodes da 4 adet olduğu için  */    
     if (currentActive > circles.length){
             currentActive = circles.length
     }
+/* update fonksiyonumuzu çağırarak currentActive'i güncelliyorum. */   
     update()
 })
 
 prev.addEventListener('click', ()=>{
     currentActive--
 
-    if (currentActive > 1){
-            currentActive = 1
+    if (currentActive < 1){
+        currentActive = 1
     }
     update()
 })
